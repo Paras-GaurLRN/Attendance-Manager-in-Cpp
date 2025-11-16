48bce1990ef0c96e309afa3ee79073465f09b182
Paras-GaurLRN <yashgaur2017@gmail.com>
2025-11-16 16:52:08 +0530

    Add English class definition in Subjects.cpp

--- Utilities/DataTypes.cpp
+++ Utilities/DataTypes.cpp
@@ -1,7 +1,7 @@
 #include <iostream> // For debugging, remove later
 #include <vector>
 
-
+/*Custom Exceptions*/
 class invalid_input : public std::exception
 {
     public:
@@ -18,9 +18,7 @@ class marks_not_assigned : public std::exception
     }
 };
 
-class STUDENT_t {
-
-};
+/*Attendance Handeling*/
 
 class ATTENDANCE_t {
     private:
@@ -43,6 +41,8 @@ class ATTENDANCE_t {
     {std::string output = (at.state) ? ("Present") : ("Absent"); return os << output;}
 };
 
+/*Subjects Base Classes*/
+
 class SUBJECT_t {
     protected:
     std::string name;
@@ -51,26 +51,79 @@ class SUBJECT_t {
 
     SUBJECT_t() = delete;
 
-    SUBJECT_t(std::string _name, unsigned short _max_marks) noexcept : name(_name), max_marks(_max_marks), marks(-1) {}
+    explicit SUBJECT_t(std::string _name, unsigned short _max_marks) noexcept : name(_name), max_marks(_max_marks), marks(-1) {}
 
     explicit SUBJECT_t(std::string _name, unsigned short _max_marks, int _marks) noexcept(false) : max_marks(_max_marks) {
         if(_marks < 0) {throw invalid_input();}
+        else if(_marks > max_marks) {throw invalid_input();}
         name = _name;
         marks = _marks;
     }
 
-    public:
+    public: // How to make it so that only called from derived classes?
     unsigned short getMarks() const noexcept(false) {
         if(marks < 0) {throw marks_not_assigned();}
         return marks;
     }
+
+    void setMarks(int _marks) noexcept(false) {
+        if(_marks < 0) {throw invalid_input();}
+        else if(_marks > max_marks) {throw invalid_input();}
+        else if(marks == -1){marks = _marks;}
+        else
+        {
+// How to make it so that this asks for password before updating marks?
+        }
+    }
 };
 
+class SUBJECT_template : public SUBJECT_t {
+    public:
+    SUBJECT_template() = delete;
+    
+    explicit SUBJECT_template(std::string _name, unsigned short _max_marks) noexcept : SUBJECT_t(_name, _max_marks) {}
+
+    explicit SUBJECT_template(std::string _name, unsigned short _max_marks, int _marks) noexcept(false)
+    : SUBJECT_t(_name, _max_marks, _marks) {}
+};
+
+/*
+template <class T>
+class SUBJECT_CONTAINER_t {
+    static_assert(std::is_base_of<SUBJECT_t, T>::value, "T must be derived from SUBJECT_t");
+    // For Compile Time Checking
+
+    private:
+    std::vector<SUBJECT_t*> subjects;
+
+    public:
+    SUBJECT_CONTAINER_t() = delete;
+
+    SUBJECT_CONTAINER_t(std::vector<SUBJECT_t*> _subjects) noexcept : subjects(_subjects) {}
+
+    friend void operator +(SUBJECT_CONTAINER_t& sc, SUBJECT_t* sub) noexcept {
+        sc.subjects.push_back(sub);
+    }
+};
+
+Yet To Be Finished
+*/
+
+/*Date Datatype Class and handeling*/
 class DATE_t{};
+
+/*Each Student Data Class Stuff*/
+class STUDENT_t {
+
+};
 
 class STUDENT_RECORD_t{};
 
+/*Making The Full Data Storage Structure (3-D Table)*/
+
 class DATA_CUBE{}; // This will ensure that data is consistent and properly connected
+
+/*Debugging Use Only*/
 
 main()
 {
--- Utilities/Subjects.cpp
+++ Utilities/Subjects.cpp
@@ -1,9 +1,4 @@
 #include "DataTypes.cpp"
 
-class English : public SUBJECT_t {
-    public:
-    English() = delete;
-    // Need to add constructor
-};
-
-// these classes will have their own static max_marks, and local marks of each student in that subject
+class ENGLISH : public SUBJECT_template {};
+class MATHEMATICS : public SUBJECT_template {};
